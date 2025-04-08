163c0cbc794902812d06c2995c7c9fda8eb3be0b
Mykhailo <mishanyadr@gmail.com>
2025-04-08 13:04:58 +0300

    fix task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,9 @@
-function formatMessage(message, maxLength){
-if(message.length >= maxLength){
+function formatMessage(message, maxLength) {
+  if (message.length > maxLength) {
     return `${message.slice(0, maxLength)}...`;
-}
-else{
+  } else {
     return message;
-}
-
+  }
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
